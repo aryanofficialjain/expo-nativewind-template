0c70994ae059ddb9d0e1d5b4741947a95e86adee
Aryan Jain <aryanofficialjain@gmail.com>
2024-09-12 18:14:43 +0530

    i am ready

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className='flex-1 text-center justify-center bg-orange-400'>
+      <Text className="text-center">Hello</Text>
+    </View>
+  )
+}
+
+export default index
